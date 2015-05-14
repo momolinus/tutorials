08a930f305ea8fe7532d72d4b84c4cd10c65e195
Marcus <m.bleil@gmx.net>
2015-05-14 16:26:39 +0200

    .classpath removed from version management

---
+++ src/main/java/de/m_bleil/javafx/binding/BindingDemo.java
@@ -0,0 +1,17 @@
+/**
+ * created at 14.05.2015 (16:20:09)
+ */
+package de.m_bleil.javafx.binding;
+
+
+/**
+ * @author Marcus Blei
+ */
+public class BindingDemo {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}

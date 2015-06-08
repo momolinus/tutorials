b0f0a08ad88af4afad6ca0d31570a6f55ea162c6
Marcus <m.bleil@gmx.net>
2015-06-08 15:43:21 +0200

    updated sample for SimpleBooleanProperty, added sample for SimpleListProperty

--- src/test/java/de/m_bleil/javafx/properties/InspectSimpleProperty.java
+++ src/test/java/de/m_bleil/javafx/properties/InspectSimpleProperty.java
@@ -3,6 +3,9 @@ package de.m_bleil.javafx.properties;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import org.junit.Test;
 
@@ -10,23 +13,47 @@ public class InspectSimpleProperty {
 
 	@Test
 	public void inspect_setValue_method_for_SimpleBooleanProperty() {
-		Boolean trueValue = Boolean.valueOf(true);
-		Boolean wrappedValue, secondWrappedValue;
+		Boolean firstTrueValue = Boolean.valueOf(true);
+		Boolean firstWrappedValue, secondWrappedValue;
 		SimpleBooleanProperty boolProperty = new SimpleBooleanProperty();
 
 		// note: stores no reference to trueValue, uses
 		// BooleanProprety#set(trueValue.getBooleanValue()) - take a look at source code
-		boolProperty.setValue(trueValue);
+		boolProperty.setValue(firstTrueValue);
 
-		wrappedValue = boolProperty.getValue();
-		assertThat(wrappedValue, is(sameInstance(trueValue)));
+		firstWrappedValue = boolProperty.getValue();
+		// because firstTrueValue was constructed with Boolean.valueOf(true)
+		assertThat(firstWrappedValue, is(sameInstance(firstTrueValue)));
 
 		// construct a new Boolean instance
-		trueValue = new Boolean(true);
-		boolProperty.setValue(trueValue);
+		Boolean secondTrueValue = new Boolean(true);
+		boolProperty.setValue(secondTrueValue);
 
+		// boolProperty "uses" for true the first created Boolean object with attribute true, here
+		// it is trueValue
 		secondWrappedValue = boolProperty.getValue();
-		assertThat(secondWrappedValue, is(not(sameInstance(trueValue))));
-		assertThat(secondWrappedValue, is(sameInstance(wrappedValue)));
+		assertThat(secondWrappedValue, is(not(sameInstance(secondTrueValue))));
+		// but
+		assertThat(secondWrappedValue, is(sameInstance(firstWrappedValue)));
+		assertThat(secondWrappedValue, is(sameInstance(firstTrueValue)));
+	}
+
+	@Test
+	public void inspect_setValue_method_for_SimpleListProperty() {
+		SimpleListProperty<String> stringListProperty = new SimpleListProperty<>();
+		ObservableList<String> stringList, oldStringList;
+
+		stringList = FXCollections.observableArrayList("item1", "item2", "item3");
+
+		// note: stores a reference to wrappedList
+		stringListProperty.set(stringList);
+		ObservableList<String> wrappedStringList = stringListProperty.get();
+		assertThat(wrappedStringList, is(sameInstance(stringList)));
+
+		stringList = FXCollections.observableArrayList("chapter1", "chapter2", "chapter3");
+		oldStringList = stringListProperty.get();
+		assertThat(stringList, is(not(sameInstance(oldStringList))));
+		assertThat(stringList, is(not(sameInstance(wrappedStringList))));
+		assertThat(oldStringList, is(sameInstance(wrappedStringList)));
 	}
 }

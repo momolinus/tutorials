73ec5bd752a164458944739d084524163da1b6ed
Marcus Bleil <marcus.bleil@tu-berlin.de>
2015-11-09 17:07:22 +0100

    removed wrong author

--- src/main/java/de/m_bleil/threads/CountThreads.java
+++ src/main/java/de/m_bleil/threads/CountThreads.java
@@ -5,15 +5,6 @@ package de.m_bleil.threads;
 
 import org.pmw.tinylog.Logger;
 
-/**
- * @author M. Comp. Sc. Marcus Bleil<br>
- *         TU Berlin<br>
- *         Arbeits-, Ingenieur- und Organisationspsychologie<br>
- *         Marchstr. 12, 10587 Berlin<br>
- *         marcus.bleil@tu-berlin.de<br>
- *         http://www.aio.tu-berlin.de
- *
- */
 public class CountThreads {
 
     /**

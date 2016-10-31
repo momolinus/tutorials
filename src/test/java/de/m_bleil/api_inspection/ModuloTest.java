82b10e779fc99dfd2cabcafe7d25c09d8767985b
momolinus <momolinus@users.noreply.github.com>
2016-10-31 07:35:22 +0100

    added inspection test for modulo %

---
+++ src/test/java/de/m_bleil/api_inspection/ModuloTest.java
@@ -0,0 +1,35 @@
+package de.m_bleil.api_inspection;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * The Class ModuloTest.
+ */
+public class ModuloTest {
+
+	@Test
+	public void testModuloNull() {
+		assertThat(0 % 1, is(equalTo(0)));
+		assertThat(0 % 2, is(equalTo(0)));
+		assertThat(0 % 3, is(equalTo(0)));
+		assertThat(0 % 5, is(equalTo(0)));
+	}
+
+	@Test
+	public void testModulo10() {
+
+		assertThat(-11 % 10, is(equalTo(-1)));
+		assertThat(-10 % 10, is(equalTo(0)));
+		assertThat(-9 % 10, is(equalTo(-9)));
+
+		assertThat(0 % 10, is(equalTo(0)));
+
+		assertThat(9 % 10, is(equalTo(9)));
+		assertThat(10 % 10, is(equalTo(0)));
+		assertThat(11 % 10, is(equalTo(1)));
+	}
+}

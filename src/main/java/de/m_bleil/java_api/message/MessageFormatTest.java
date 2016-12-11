9c554d45c1b159e23d5cbe634aabe1d2e8cb83a5
momolinus <momolinus@users.noreply.github.com>
2016-12-11 14:43:02 +0100

    added inspection test for java.text.MessageFormat.format
    - inspected how quoting with ' works

---
+++ src/main/java/de/m_bleil/java_api/message/MessageFormatTest.java
@@ -0,0 +1,31 @@
+/**
+ * 11.12.2016
+ */
+package de.m_bleil.java_api.message;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import java.text.MessageFormat;
+
+import org.junit.Test;
+
+/**
+ * @author Marcus Bleil, www.marcusbleil.de
+ */
+public class MessageFormatTest {
+
+	@Test
+	public void testQuoting_01() {
+		String result = MessageFormat.format("text1 ''{0}'' text3", "text2");
+
+		assertThat(result, is(equalTo("text1 'text2' text3")));
+	}
+
+	@Test
+	public void testQuoting_02() {
+		String result = MessageFormat.format("text1 '{0}' text3", "text2");
+
+		assertThat(result, is(equalTo("text1 {0} text3")));
+	}
+}

8a7607a2c3ad019e5cb2e111150462085c72caeb
momolinus <momolinus@users.noreply.github.com>
2016-12-14 08:47:57 +0100

    more inspection tests

--- src/main/java/de/m_bleil/java_api/datetime/DateTimeFormatterTest.java
+++ src/main/java/de/m_bleil/java_api/datetime/DateTimeFormatterTest.java
@@ -9,13 +9,19 @@ import static org.hamcrest.Matchers.*;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * @author Marcus Bleil, www.marcusbleil.de
  */
 public class DateTimeFormatterTest {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
 	@Test
 	public void testFormatter_dd_LL_YYYY() {
@@ -45,4 +51,29 @@ public class DateTimeFormatterTest {
 
 		assertThat(dateString, is(equalTo("30-Sep-1949")));
 	}
+
+	@Test
+	public void testFormatter_dd_MMM_aYear_as_parser_01() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-YYYY");
+
+		expectedException.expect(DateTimeParseException.class);
+		LocalDate.parse("9-Sep-1949", formatter);
+	}
+
+	@Test
+	public void testFormatter_dd_MMM_aYear_as_parser_02() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+
+		expectedException.expect(DateTimeParseException.class);
+		LocalDate.parse("9-Sep-1949", formatter);
+	}
+
+	@Test
+	public void testFormatter_dd_MMM_aYear_as_parser_03() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-uuuu");
+
+		expectedException.expect(DateTimeParseException.class);
+		LocalDate.parse("9-Sep-1949", formatter);
+	}
+
 }

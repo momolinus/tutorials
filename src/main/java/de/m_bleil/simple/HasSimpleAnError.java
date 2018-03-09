14e9a651a2e84c1f4557ecac57ffcaef7669e130
momolinus <momolinus@users.noreply.github.com>
2018-03-09 16:49:51 +0100

    note to myself

---
+++ src/main/java/de/m_bleil/simple/HasSimpleAnError.java
@@ -0,0 +1,17 @@
+/**
+ * created at 09.03.2018
+ */
+package de.m_bleil.simple;
+
+
+/**
+ * ist es ein Problem für Simple wenn:
+ * <element>30
+ * </element>
+ *
+ * @author Marcus Bleil, www.m-bleil.de
+ *
+ */
+public class HasSimpleAnError {
+
+}
